f940e49f301b731804a203f11dc535dfef700ea2
MPreston <michael_n_preston@outlook.com>
2018-04-18 01:19:04 -0500

    Added  & Moved TPOT Mobile
    Started a Android project (needs Hyper-V installed to host machine).  Added TPOT Banner.png.

---
+++ TPOT Mobile/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TPOT_Mobile
+{
+    [Activity(Label = "TPOT_Mobile", MainLauncher = true)/*, Icon = "@drawable/icon"*/]
+    public class MainActivity : Activity
+    {
+        int count = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            //get button from the layout resource:
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+            //button.Click += delegate
+            //{
+            //    button.Text = $"{count++} clicks";
+            //};
+        }
+    }
+}
+
