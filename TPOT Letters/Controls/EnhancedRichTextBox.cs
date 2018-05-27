be5f36ad787ac56ea979ee80cda52a25f7e0a7f3
MPreston <michael_n_preston@outlook.com>
2018-05-27 16:18:12 -0500

    Added 'enhanced' richtextbox from Stack Overflow

---
+++ TPOT Letters/Controls/EnhancedRichTextBox.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Windows.Documents;
+
+namespace System.Windows.Controls
+{
+    class EnhancedRichTextBox : RichTextBox
+    {
+        public string File
+        {
+            get { return (string)GetValue(FileProperty); }
+            set { SetValue(FileProperty, value); }
+        }
+
+        public static DependencyProperty FileProperty =
+            DependencyProperty.Register("File", typeof(string), typeof(EnhancedRichTextBox),
+            new PropertyMetadata(new PropertyChangedCallback(OnFileChanged)));
+
+        private static void OnFileChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            EnhancedRichTextBox rtf = d as EnhancedRichTextBox;
+            if (d == null)
+            {
+                return;
+            }
+
+            ReadFile(rtf.File, rtf.Document);
+        }
+
+        private static void ReadFile(string inFilename, FlowDocument inFlowDocument)
+        {
+            if (System.IO.File.Exists(inFilename))
+            {
+                TextRange range = new TextRange(inFlowDocument.ContentStart, inFlowDocument.ContentEnd);
+                FileStream fStream = new FileStream(inFilename, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+                range.Load(fStream, DataFormats.Rtf);
+                fStream.Close();
+            }
+        }
+    }
+}
---
+++ TPOT Letters/Controls/HyperlinkNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TPOTLetters
+{
+    public class Hyperlink
+    {
+        public static bool NavigateToUrl(RoutedEventArgs args)
+        {
+            object source = args?.OriginalSource;
+
+            try
+            {
+                if (source is System.Windows.Documents.Hyperlink link)
+                {
+                    Process.Start(link?.NavigateUri?.ToString());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return true;
+        }
+    }
+}
