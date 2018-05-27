9e74e54745b8d6e57573ab1610c0bce7c3e5d4b1
MPreston <michael_n_preston@outlook.com>
2018-05-27 16:20:35 -0500

    Removed

--- TPOT Letters/ViewModels/HtmlAwareViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared;
-
-namespace TPOTLetters
-{
-    public abstract class TextAwareViewModel : ViewModelBase, IViewSubscriber
-    {
-        public abstract void Update(string html);
-    }
-}
