66ffd00eb61a50b0871015b806bf9a029dcaff5c
MPreston <michael_n_preston@outlook.com>
2018-06-30 15:27:03 -0500

    Removed class definition from RTFConverter
    Can make class later, once we figure out how to extract values from inside local scopes.

--- TPOT Electron/assets/js/RtfConverter.js
+++ TPOT Electron/assets/js/RtfConverter.js
@@ -1,51 +1,6 @@
-const letterPath = './assets/SampleLetter.rtf'
+const letterPath = './assets/rtf/SampleLetter.rtf'
 const rtfToHtml = require('@iarna/rtf-to-html')
 var fs = require('fs')
-
-class RtfConverter {
-    // constructor(path) {
-    //     this.filePath = path
-    // }
-
-    setPath(path) {
-        this.filePath = rtfFilePath
-    }
-
-    convert(path) {
-        // if (path) {
-        //     throw "rtf file path cannot be empty!"
-        // }
-        fs.createReadStream(path).pipe(rtfToHtml((error, html) => {
-            // console.log(html)
-            this.html = html
-        }))
-        console.log(this.html)
-    }
-
-    static convert(path) {
-        if (!this.path) {
-            throw "rtf file path cannot be empty!"
-        }
-
-        fs.createReadStream(path).pipe(rtfToHtml((error, html) => {
-            console.log(html)
-            return html
-        }))
-    }
-
-    //todo: delete when done testing
-    static convertSample() {
-        console.log('started conversion')
-        fs.createReadStream(letterPath).pipe(rtfToHtml((error, html) => {
-            console.log('Conversion complete')
-            console.log(html)
-            return html
-        }))
-
-        console.log('convertsample(): \n', result)
-        return result
-    }
-}
 
 function sampleCallback() {
     var x = (function () {
@@ -54,42 +9,36 @@ function sampleCallback() {
     console.log(x)
 }
 
-
-function usageSample() {
-    let htmlOuter
-    fs.createReadStream('example.rtf').pipe(rtfToHTML((err, html) => {
-        console.log(html)
-        htmlOuter = html
-    }))
-    console.log(htmlOuter)
-}
+// function usageSample() {
+//     let htmlOuter
+//     fs.createReadStream('example.rtf').pipe(rtfToHTML((err, html) => {
+//         console.log(html)
+//         htmlOuter = html
+//     }))
+//     console.log(htmlOuter)
+// }
 
 
 //todo: delete when done testing
 function convertSample() {
-    // console.log('started conversion')
-    let callback = function (err, html) {
+    console.log('started conversion')
+    var regexp = new RegExp('rtf')
+    fs.createReadStream(letterPath).pipe(rtfToHtml((err, html) => {
         console.log('callback-inner():\n', html)
-        // this.html = html
-        // return html
-    }
+        savePath = letterPath.replace(regexp, 'html')
+        console.log(savePath)
 
-    fs.createReadStream(letterPath).pipe(rtfToHtml(callback))
-
-
-    console.log('callback-outer()', callback.html)
-    var x = callback();
-    console.log('x = ', x)
-
+        //ERROR: Works, but we can't get our save path back to access it.
+        // fs.writeFile(savePath, html, (error) => {
+        //     if (error) {
+        //         return console.log(error)
+        //     }
+        //     console.log('save complete')
+        // })
+    }))
 }
 
-// foo("", function (html) {
-//     console.log(html)
-// });
-
-
 module.exports = {
-    RtfConverter,
     convertSample,
     sampleCallback,
 }
