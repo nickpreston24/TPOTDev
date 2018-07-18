112556315e2354093c25f122969bf0dcabbfa145
MPreston <michael_n_preston@outlook.com>
2018-07-18 13:57:45 -0500

    Added Repository pattern

---
+++ Shared/Classes/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace Shared
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected IDbConnection database;
+
+        protected Repository(string connectionString)
+        {
+            database = new SqlConnection(connectionString);
+        }
+
+        protected Repository(IDbConnection database)
+        {
+            this.database = database;
+        }
+
+        public abstract TEntity Add(TEntity entity);
+        public abstract IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities);
+        public abstract void Dispose();
+        public abstract IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        public abstract TEntity Get(int id);
+        public abstract IEnumerable<TEntity> GetAll();
+        public abstract TEntity Remove(TEntity entity);
+        public abstract IEnumerable<TEntity> RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ Shared/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Shared
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        TEntity Add(TEntity entity);
+        IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities);
+        TEntity Remove(TEntity entity);
+        IEnumerable<TEntity> RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
