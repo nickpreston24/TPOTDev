000824ef262e2bd98f6ddf81ad642dfef2d91a97
MPreston <michael_n_preston@outlook.com>
2018-07-25 00:09:35 -0500

    Added new Extensions

---
+++ Shared/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    public static class DictionaryExtensions
+    {
+        public static TV GetValue<TK, TV>(this IDictionary<TK, TV> dictionary, TK key, TV defaultValue = default(TV))
+        {
+            return dictionary.TryGetValue(key, out TV value) ? value : defaultValue;
+        }
+    }
+}
--- Shared/Extensions/ObjectExtensions.cs
+++ Shared/Extensions/ObjectExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace Shared
+namespace System
 {
     public static class ObjectExtensions
     {
