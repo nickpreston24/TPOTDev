29597476b6befd37ae9ca8f348fc21c6a626c95a
MPreston <michael_n_preston@outlook.com>
2018-09-04 22:57:24 -0500

    Added example of setting properties with Promise, lest (Michael) forgets.

---
+++ TPOT Electron/test/archived/setterTests.js
@@ -0,0 +1,47 @@
+var expect = require('chai').expect;
+
+// var cat = (name) => {
+//     this.name = name
+// }
+
+function Cat(name) {
+    this.name = name;
+
+    this.setName = async (name) => {
+        // return new Promise((result, reject) => {
+        //     setTimeout(() => {
+        //         this.name = name;
+        //     }, 5000)
+        // });
+
+        sleep(() => {
+            console.log('inside the sleep()');
+            this.name = name;
+        }, null, 3000);
+
+    }
+
+    function timeout(milliseconds) {
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+
+    async function sleep(func, args, ms) {
+        const response = await timeout(ms);
+        return func(args);
+    }
+
+}
+
+describe('kitty', () => {
+
+    it('should print name', async () => {
+
+        var cat = new Cat('Heathcliff');
+        console.log(cat.name);
+
+        await cat.setName('Barney');
+        console.log(cat.name);
+
+    });
+
+});
