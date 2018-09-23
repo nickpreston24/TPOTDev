37d49457f44e4f7820d282825b475aea264aa484
mikepreston17 <michael.n.preston@gmail.com>
2018-09-22 22:57:15 -0500

    init

---
+++ TPOT Electron/src/modules/firebase/FirebaseDb.js
@@ -0,0 +1,8 @@
+class FirebaseDb {
+    //Takes a json, identifies what collections or documents it matches from the db, then adds them
+    //Throws error if collection or document cannot be found.
+    //OR, just store as a new collection (might be volatile)
+    async add(json) {
+        //todo: figure out how to parse json objects.        
+    }
+}
