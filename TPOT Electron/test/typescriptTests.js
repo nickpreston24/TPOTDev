876d321ad0c470e03a3f375d85e35cec3ccf9d05
mikepreston17 <michael.n.preston@gmail.com>
2018-10-07 12:32:29 -0500

    updated to use clipboard as a test

--- TPOT Electron/test/typescriptTests.js
+++ TPOT Electron/test/typescriptTests.js
@@ -1,16 +1,17 @@
-var expect = require('chai').expect;
+// var expect = require('chai').expect;
 
-// import DiskFileLoader from '../src/modules/docxLoaders/DiskFileLoader.ts'
-// var DiskFileLoader = require('../src/modules/docxLoaders/DiskFileLoader.ts')
-// const Clipboard = require("../modules/docxLoaders_ts/Clipboard.ts").ClipBoard;
+// import {loader} from '../src/modules/docxLoaders_ts/DiskFileLoader.ts'
 
-describe.skip('js-calls-typescript test', () => {
-    // const Clipboard = require('../src/modules/docxLoaders_ts/Clipboard.ts').Clipboard;
-    it('should let us call defined TS classes from vanilla JS', () => {
-        // let loader = new DiskFileLoader();
-        // if (!loader) console.log('could not create instance');
-        // else console.log('success!');
-        // let loader = new Clipboard();
-        expect(loader).to.be.not.null();
-    });
-});
+const loader  = require('../src/modules/docxLoaders_ts/Clipboard.ts');
+// // const Clipboard = require("../modules/docxLoaders_ts/Clipboard.ts").ClipBoard;
+
+// describe('js-calls-typescript test', () => {
+//     // const Clipboard = require('../src/modules/docxLoaders_ts/Clipboard.ts').Clipboard;
+//     it('should let us call defined TS classes from vanilla JS', () => {
+//         let loader = new DiskFileLoader();
+//         // if (!loader) console.log('could not create instance');
+//         // else console.log('success!');
+//         // let loader = new Clipboard();
+//         expect(loader).to.be.not.null();
+//     });
+// });
