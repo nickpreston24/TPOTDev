67e60cbb2ac4e4c96f2aee8f80c8416f5deac98c
Braden-Preston <bpfilmsinc@gmail.com>
2018-10-10 01:19:44 -0500

    release 0.0.12

--- TPOT Electron/public/electron.js
+++ TPOT Electron/public/electron.js
@@ -4,34 +4,129 @@ const BrowserWindow = electron.BrowserWindow;
 const app = electron.app;
 const ipc = electron.ipcMain
 const dialog = electron.dialog
-const {
-    autoUpdater
-} = require("electron-updater");
+const autoUpdater = require("electron-updater").autoUpdater;
 // require('electron-debug')() // Doesn't work?
-
-
 
 // Other Modules
 const fs = require("fs");
 const path = require("path");
 const isDev = require("electron-is-dev");
 const log = require('electron-log')
-
+const chalk = require('chalk');
 
 let updater
+let toolboxWindow;
 autoUpdater.autoDownload = false
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
-// Main Process
-let toolboxWindow;
+
+//  ELECTRON MAIN PROCESS APP EVENTS
+/////////////////////////////////////////////////////////////////////////
+
+app.on("ready", () => {
+    createToolboxWindow()
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (toolboxWindow === null) {
+        createWindow(offset);
+    }
+});
 
 
-// require("update-electron-app")({
-//     repo: "kitze/react-electron-example",
-//     updateInterval: "1 hour"
-// });
+//  TOOLBOX  POST-LOAD EVENTS
+/////////////////////////////////////////////////////////////////////////
+
+ipc.on("toolbox-initialized", (event, arg) => {
+    console.log(chalk.bgGreen.black('Toolbox render process loaded...'));
+    // Start Auto Update Service
+    autoUpdater.checkForUpdates()
+    setInterval(() => {
+        autoUpdater.checkForUpdates()
+    }, 60000)
+})
+
+
+//  AUTO UPDATES
+/////////////////////////////////////////////////////////////////////////
+
+autoUpdater.on('checking-for-update', () => {
+    sendUpdateStatusToToolbox('update-checking', 'Checking for update...')
+})
+
+autoUpdater.on('update-available', (info) => {
+    sendUpdateStatusToToolbox('update-available', `Found Update: ${info.version}`, info)
+})
+
+autoUpdater.on('update-not-available', (info) => {
+    sendUpdateStatusToToolbox('update-not-available', 'Update not available.', info)
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+    sendUpdateStatusToToolbox('update-downloaded', `Update downloaded: ${info.version}`, info)
+})
+
+autoUpdater.on('error', (err) => {
+    sendUpdateStatusToToolbox('update-error', `Error: ${err}`, err)
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+    sendUpdateStatusToToolbox('update-download-progress', `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred}/${progressObj.total})`, info)
+})
+
+ipc.on("update-confirm-download-and-restart", (event, arg) => {
+    autoUpdater.quitAndInstall(); // Downloads and Quits
+})
+
+ipc.on("update-confirm-download", (event, arg) => {
+    autoUpdater.downloadUpdate(); // Just Downloads
+})
+
+ipc.on("update-confirm-restart", (event, arg) => {
+    autoUpdater.quitAndInstall(); // Assuming already download, will just quit, otherwise, download  quit
+})
+
+function sendUpdateStatusToToolbox(event, desc, data) {
+    log.info(desc ? desc : "auto-update uknown event triggered");
+    toolboxWindow.webContents.send('auto-update', msg = {
+        event: event,
+        desc: desc,
+        data: data ? data : null
+    });
+}
+
+
+//  CONFIG AND UTILITIES
+/////////////////////////////////////////////////////////////////////////
+
+const createToolboxWindow = () => {
+    // Create Main window (Refactor Later)
+    var electronScreen = electron.screen;
+    var displays = electronScreen.getAllDisplays();
+    var externalDisplay = null;
+    for (var i in displays) {
+        if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
+            externalDisplay = displays[i];
+            break;
+        }
+    }
+
+    if (externalDisplay) {
+        let offset = {
+            x: externalDisplay.bounds.x + 60,
+            y: externalDisplay.bounds.y + 170
+        }
+        createWindow(offset)
+    }
+}
 
 function createWindow(offset) {
     console.log(offset)
@@ -77,99 +172,23 @@ function createWindow(offset) {
 
 }
 
-app.on("ready", () => {
+// autoUpdater.on('update-downloaded', (info) => {
+//     autoUpdater.quitAndInstall();
+// })
 
-    // Create Main window (Refactor Later)
-    var electronScreen = electron.screen;
-    var displays = electronScreen.getAllDisplays();
-    var externalDisplay = null;
-    for (var i in displays) {
-        if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
-            externalDisplay = displays[i];
-            break;
-        }
-    }
-
-    if (externalDisplay) {
-        let offset = {
-            x: externalDisplay.bounds.x + 60,
-            y: externalDisplay.bounds.y + 170
-        }
-        createWindow(offset)
-    }
-
-    // // Start Auto-Update Service
-    // autoUpdater.checkForUpdates();
-    toolboxWindow.send('test')
-
-});
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-});
-
-app.on("activate", () => {
-    if (toolboxWindow === null) {
-        createWindow(offset);
-    }
-});
-
-// Auto Update Event Callbacks
-function sendStatusToWindow(text) {
-    log.info(text);
-    toolboxWindow.webContents.send('message', text);
-}
-
-// IPC Dispatch
-autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-    dialog.showMessageBox({
-        type: 'info',
-        title: 'Found Updates',
-        message: 'Found updates, do you want update now?',
-        buttons: ['Sure', 'No']
-    }, (buttonIndex) => {
-        if (buttonIndex === 0) {
-            autoUpdater.downloadUpdate()
-        } else {
-            updater.enabled = true
-            updater = null
-        }
-    })
-})
-autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
-    dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
-})
-autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-    autoUpdater.quitAndInstall();
-})
-autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
-});
-
-// IPC Listen
-ipc.on("quitAndInstall", (event, arg) => {
-    autoUpdater.quitAndInstall();
-})
-
-ipc.on("render", (event, arg) => {
-    console.log("RENDER-M")
-    // Start Auto-Update Service
-    autoUpdater.checkForUpdates();
-    toolboxWindow.webContents.send('message', 'hey there')
-})
+// autoUpdater.on('update-available', (info) => {
+//     sendStatusToWindow('Update available.');
+//     dialog.showMessageBox({
+//         type: 'info',
+//         title: `Found Updates ${info}`,
+//         message: 'Found updates, do you want update now?',
+//         buttons: ['Sure', 'No']
+//     }, (buttonIndex) => {
+//         if (buttonIndex === 0) {
+//             autoUpdater.downloadUpdate()
+//         } else {
+//             updater.enabled = true
+//             updater = null
+//         }
+//     })
+// })
--- TPOT Electron/src/index.js
+++ TPOT Electron/src/index.js
@@ -5,24 +5,23 @@ import Toolbox from './apps/Toolbox';
 import './index.css';
 
 // Electron
-window.require('electron-react-devtools').install() // Works, but resets
-window.require('devtron').install() // Not Working ATM
+// window.require('electron-react-devtools').install() // Works, but resets
+// window.require('devtron').install() // Not Working ATM
 const electron = window.require('electron')
-const remote = electron.remote;
-const app = remote.app
-const electron_process = electron.process;
-const electron_app = electron.app;
-const ipc = electron.ipcRenderer;
+// const remote = electron.remote;
+// const app = remote.app
+// const electron_process = electron.process;
+// const electron_app = electron.app;
+// const ipc = electron.ipcRenderer;
 
-ipc.on('message', function (event, text) {
-    console.log(event, text)
-})
+const ipc = window.require('electron').ipcRenderer;
+
 
 // ipc.on('test', function (event, text) {
 //     console.log("TEST-R")
 // })
 
-ipc.send('render')
+
 
 // ipc.on('render', function (event, text) {
 //     console.log("RENDER- M")
@@ -36,6 +35,22 @@ ipc.send('render')
 // })
 
 
-
 // Initialization of React Toolbox Application into Index.js Render Process Browser Window
 ReactDOM.render(<Toolbox />, document.getElementById('root'));
+
+// Initialize Auto Update Service
+ipc.send('toolbox-initialized')
+
+ipc.on('auto-update', function (e, msg) {
+    console.log(msg)
+})
+
+// function sendUpdateStatusToToolbox(event, desc, data) {
+//     let message = {
+//         event: event,
+//         desc: desc,
+//         data: data ? data : null
+//     }
+//     log.info(desc ? desc : "auto-update uknown event triggered");
+//     toolboxWindow.webContents.send('auto-update', text);
+// }
