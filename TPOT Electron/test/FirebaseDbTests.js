e4289344573cc88be475209aad8e55babd4317e9
mikepreston17 <michael.n.preston@gmail.com>
2018-10-23 16:10:04 -0500

    updated tests

---
+++ TPOT Electron/test/FirebaseDbTests.js
@@ -0,0 +1,35 @@
+const admin = require('firebase-admin');
+const config = require('../src/config/environment.js').config;
+var serviceAccount = require('../src/config/service-account.json');
+console.log('Service Account:\n', serviceAccount);
+
+// var app = admin.initializeApp(config);
+var app = admin.initializeApp(serviceAccount)
+var db = admin.firestore();
+
+
+// console.log('Hello from db tests');
+
+
+
+// given code:
+// const firestore = new Firestore();
+// const settings = {
+//     config,
+//     timestampsInSnapshots: true
+// };
+// firestore.settings(settings);
+
+
+
+// console.log('app', app);
+// console.log('db', db);
+
+var docRef = db.collection('users').doc('alovelace');
+console.log('docref\n', docRef);
+
+var setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    // born: 1815
+});
---
+++ TPOT Electron/test/FirebaseTests.js
@@ -0,0 +1,63 @@
+/*
+ * Firestore
+ */
+var firebase = require("firebase/app");
+require("firebase/auth");
+const config = require('../src/config/environment.js').config;
+var app = firebase.initializeApp(config);
+
+/**
+ * constants
+ */
+const email = "michael.n.preston@gmail.com";
+const password = "Mercury10";
+
+describe("sign in", () => {
+    it('should sign in', () => {
+        login();
+    })
+})
+
+async function login() {
+    return app.auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(console.log);
+}
+
+
+// firebase.initializeApp({
+//     serviceAccount: '../src/config/service-account.json',
+//     databaseUrl: 'https://tpot-toolbox.firebaseio.com/',
+// });
+
+// describe.skip("Sign In", () => {
+//     it('should sign in with no error', () => {
+//         var authenticator = new FirebaseAuthenticator(email, password);
+//         authenticator.login().then((result) => {
+//             // console.log('result: ', result);
+//         }).catch(console.log);
+//     })
+// });
+
+// describe.skip("Add Data", () => {
+
+//     var ref = firebase.app().database().ref();
+
+//     // var users = db.collection('users').doc('Michael');
+
+//     // var setMichael = users.set({
+//     //     FirstName: 'Michael',
+//     //     LastName: 'Preston',
+//     //     Email: 'michael.n.preston@gmail.com',
+//     //     password: 'Mintsharp18!',
+//     // }).catch(console.log)
+
+
+//     // var app = authenticator.app;
+//     // var db = app.db;
+
+//     //  console.log('retrived\n', authenticator.app);
+//     // var users = db.collection('users');
+
+//     // console.log('users:\n', users);
+// })
