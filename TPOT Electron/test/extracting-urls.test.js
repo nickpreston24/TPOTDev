57ce1d67527414ed35ae48977ea3c078d9fa42a8
Braden Preston <bpfilmsinc@gmail.com>
2019-02-12 02:48:58 -0600

    0.1.1 Release

--- TPOT Electron/test/extracting-urls.test.js
+++ TPOT Electron/test/extracting-urls.test.js
@@ -1,6 +1,5 @@
 var assert = require('assert');
 var chai = require('chai');
-chai.use(require('chai-string'));
 var expect = require('expect');
 
 // import * as strategies from '../src/editor/plugins/draft-js-link-decorators/utils/strategies.jsx';
