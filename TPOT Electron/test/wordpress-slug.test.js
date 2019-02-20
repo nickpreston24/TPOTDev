03326e89efb969edb0f7488fd8513153b9cb2364
Michael Preston <michael.n.preston@gmail.com>
2019-02-20 14:31:02 -0600

    0.1.5 Windows Release

---
+++ TPOT Electron/test/wordpress-slug.test.js
@@ -0,0 +1,13 @@
+// const wp = require('src/wordpress');
+// require('./src//wordpress');
+
+// import { createPage } from '../src/wordpress';//I don't think imports work from js vanilla
+
+let wp = require('../src/wordpress');
+
+describe('test', () => {
+    console.log('hi')
+    console.log( wp.createPage )
+})
+
+//
